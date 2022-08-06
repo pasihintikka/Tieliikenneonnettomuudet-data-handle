5241a32184ebdf2351cd1355f7c2378b59d310b6
pasihintikka <110663840+pasihintikka@users.noreply.github.com>
2022-08-06 11:59:58 +0300

    Add files via upload

---
+++ SQL-files/SQL_Create_Procedures.sql
@@ -0,0 +1,211 @@
+-- SQL SERVER PROCEDURES
+
+--use Onnettomuudet
+
+--------------------------------------------
+-- BASE QUERY INCIDENT WITH INJURED INVOLVED
+-- BY SELECTED YEAR
+
+/*
+CREATE OR ALTER PROCEDURE uspGetLoukk_Osall (@vuosi INT)
+AS 
+BEGIN
+    SELECT
+        Vuosi,
+		o.Onnett_id AS Onnett_nro,
+		Onlksel AS Onnett_luokka,
+		Onnpaiksel AS Onnettomuuspaikka,
+		Loukk AS Loukkantuneita,
+		Oslajisel AS Osallisen_laji
+    FROM 
+        Onnettomuudet o
+	INNER JOIN Osalliset os
+        ON o.Onnett_id = os.Onnett_id
+    WHERE
+        Vuosi = @vuosi AND
+		Loukkaant > 0
+    ORDER BY 
+        Vuosi,
+		o.Onnett_id,
+		Onlksel,
+		Onnpaiksel,
+		Loukk,
+		Oslajisel;
+END;
+*/
+
+-- CHECK PROCEDURE
+/*
+EXEC uspGetLoukk_Osall 2017
+*/
+
+-----------------------------------------
+-- BASE QUERY INCIDENT WITH DEAD INVOLVED
+-- BY SELECTED YEAR
+
+/*
+CREATE OR ALTER PROCEDURE uspGetKuoll_Osall (@vuosi INT)
+AS 
+BEGIN
+    SELECT
+        Vuosi,
+		o.Onnett_id AS Onnett_nro,
+		Onlksel AS Onnett_luokka,
+		Onnpaiksel AS Onnettomuuspaikka,
+		Kuollut AS Kuolleita,
+		Loukk AS Loukkaantuneita,
+		Oslajisel AS Osallisen_laji
+    FROM 
+        Onnettomuudet o
+	INNER JOIN Osalliset os
+        ON o.Onnett_id = os.Onnett_id
+    WHERE
+        Vuosi = @vuosi AND
+		Kuolleet > 0
+    ORDER BY 
+        Vuosi,
+		o.Onnett_id,
+		Onlksel,
+		Onnpaiksel,
+		Kuollut,
+		Loukk,
+		Oslajisel;
+END;
+*/
+
+-- CHECK PROCEDURE
+/*
+EXEC uspGetKuoll_Osall 2017
+*/
+
+------------------------------------------------------------
+-- QUERY INCIDENT SERIOUS WITH INCIDENT TYPES AND CONDITIONS
+-- WITHOUT MILD INCIDENT BY SELECTED YEAR
+
+/*
+CREATE OR ALTER PROCEDURE uspGetVakOnnett_Olos (@vuosi INT)
+AS 
+BEGIN
+    SELECT
+        o.Vuosi,
+	    Kk AS Kuukausi,
+	    Vkpv AS Viikonpäivä,
+	    Tunti,
+	    Vakavuus,
+	    Loukkaant AS Loukkaantuneet,
+	    Kuolleet,
+	    Onlksel AS Onnett_Luokka,
+	    Ontyypsel AS Onnett_Tyyppi,
+	    Onnpaiksel AS Onnett_Paikka,
+	    Nopraj AS Nopeusrajoitus,
+	    t.Noplajisel AS NopRaj_Laji,
+	    t.Risteys AS Risteävien_lkm,
+	    t.Risteyssel AS Risteys_Laji,
+	    Pintasel AS Tienpinta,
+	    Valsel AS Valoisuus,
+	    Sääsel AS Sää,
+	    Lämpötila
+    FROM
+        Onnettomuudet o
+    INNER JOIN Tieomin t
+        ON o.tieomin_id = t.tieomin_id AND
+	    o.Vuosi = t.Vuosi
+	WHERE
+        o.Vuosi = @vuosi AND
+		Vakavuusko > 0
+    GROUP BY 
+        o.Vuosi,
+	    Kk,
+	    Vkpv,
+	    Tunti,
+	    Vakavuus,
+	    Loukkaant,
+	    Kuolleet,
+	    Onlksel,
+	    Ontyypsel,
+	    Onnpaiksel,
+	    Nopraj,
+	    t.Noplajisel,
+	    t.Risteys,
+	    t.Risteyssel,
+	    Pintasel,
+	    Valsel,
+	    Sääsel,
+	    Lämpötila;
+END;
+*/
+
+-- CHECK PROCEDURE
+/*
+EXEC uspGetVakOnnett_Olos 2017
+*/
+
+----------------------------------------------------------------------
+-- QUERY INCIDENT SERIOUS WITH INCIDENT TYPES, CONDITIONS AND LOCATION
+-- WITHOUT MILD INCIDENT BY SELECTED YEAR
+
+/*
+CREATE OR ALTER PROCEDURE uspGetVakOnnett_Paikka (@vuosi INT)
+AS 
+BEGIN
+    SELECT
+        o.Vuosi,
+	    Kk AS Kuukausi,
+	    Vkpv AS Viikonpäivä,
+	    Tunti,
+	    Vakavuus,
+	    Loukkaant AS Loukkaantuneet,
+	    Kuolleet,
+	    Onlksel AS Onnett_Luokka,
+	    Ontyypsel AS Onnett_Tyyppi,
+	    Onnpaiksel AS Onnett_Paikka,
+		Nopraj AS Nopeusrajoitus,
+	    Pintasel AS Tienpinta,
+	    Valsel AS Valoisuus,
+	    Sääsel AS Sää,
+	    Lämpötila,
+	    a.Maakuntsel AS Maakunta,
+		Väestö,
+	    a.Kuntasel AS Kunta,
+	    Katuosoite,
+	    [position.lat],
+	    [position.lon]
+    FROM
+        Onnettomuudet o
+    INNER JOIN Alueet a
+        ON o.alue_id = a.alue_id
+	INNER JOIN Väestö v
+        ON o.Vuosi = v.Vuosi
+	WHERE
+        o.Vuosi = @vuosi AND
+		a.Maakuntsel = v.Maakuntsel AND
+		Vakavuusko > 0
+    GROUP BY 
+        o.Vuosi,
+	    Kk,
+	    Vkpv,
+	    Tunti,
+		Vakavuus,
+	    Loukkaant,
+	    Kuolleet,
+	    Onlksel,
+	    Ontyypsel,
+	    Onnpaiksel,
+		Nopraj,
+	    Pintasel,
+	    Valsel,
+	    Sääsel,
+	    Lämpötila,
+	    a.Maakuntsel,
+		Väestö,
+	    a.Kuntasel,
+	    Katuosoite,
+	    [position.lat],
+	    [position.lon];
+END;
+*/
+
+-- CHECK PROCEDURE
+/*
+EXEC uspGetVakOnnett_Paikka 2017
+*/
---
+++ SQL-files/SQL_Create_Views.sql
@@ -0,0 +1,279 @@
+-- SQL SERVER VIEWS
+
+--use Onnettomuudet
+
+----------------------------
+-- BASE QUERY COUNT INCIDENT
+
+/*
+CREATE OR ALTER VIEW vuos_maak_onnett (
+    Vuosi,
+	Maakunta,
+	Väestö,
+	lkm_onnett, 
+    lkm_kuolleet,
+    lkm_loukkaant
+)
+AS
+SELECT
+    o.Vuosi,
+	a.Maakuntsel,
+	Väestö,
+	COUNT(Onnett_id) lkm_onnett, 
+    SUM(Kuolleet) lkm_kuolleet,
+    SUM(Loukkaant) lkm_loukkaant
+FROM
+    Onnettomuudet o
+INNER JOIN Alueet a
+        ON o.alue_id = a.alue_id
+INNER JOIN Väestö v
+        ON o.Vuosi = v.Vuosi
+WHERE
+	a.Maakuntsel = v.Maakuntsel
+GROUP BY 
+    o.Vuosi,
+	a.Maakuntsel,
+	Väestö;
+*/
+
+-- CHECK VIEW QUERY
+/*
+SELECT *
+FROM
+   vuos_maak_onnett;
+*/
+
+---------------------------------------------------
+-- BASE QUERY INCIDENT SERIOUS WITH INCIDENT TYPES
+
+/*
+CREATE OR ALTER VIEW vuos_maak_onnett_tyyp
+AS
+SELECT
+    o.Vuosi,
+    a.Maakuntsel AS Maakunta,
+    Väestö,
+    Vakavuus,
+    Onlksel AS Onnett_Luokka,
+    Ontyypsel AS Onnett_Tyyppi
+FROM
+    Onnettomuudet o
+INNER JOIN Alueet a
+    ON o.alue_id = a.alue_id
+INNER JOIN Väestö v
+    ON o.Vuosi = v.Vuosi
+WHERE
+	a.Maakuntsel = v.Maakuntsel
+GROUP BY 
+    o.Vuosi,
+    a.Maakuntsel,
+	Väestö,
+    Vakavuus,
+    Onlksel,
+    Ontyypsel;
+*/
+
+-- CHECK VIEW QUERY
+/*
+SELECT *
+FROM
+   vuos_maak_onnett_tyyp;
+*/
+
+------------------------------------------------------------
+-- QUERY INCIDENT SERIOUS WITH INCIDENT TYPES AND CONDITIONS
+-- WITHOUT MILD INCIDENT
+
+/*
+CREATE OR ALTER VIEW vuos_vak_onnett_olos
+AS
+SELECT
+    o.Vuosi,
+	Kk AS Kuukausi,
+	Vkpv AS Viikonpäivä,
+	Tunti,
+	Vakavuus,
+	Loukkaant AS Loukkaantuneet,
+	Kuolleet,
+	Onlksel AS Onnett_Luokka,
+	Ontyypsel AS Onnett_Tyyppi,
+	Onnpaiksel AS Onnett_Paikka,
+	Nopraj AS Nopeusrajoitus,
+	t.Noplajisel AS NopRaj_Laji,
+	t.Risteys AS Risteävien_lkm,
+	t.Risteyssel AS Risteys_Laji,
+	Pintasel AS Tienpinta,
+	Valsel AS Valoisuus,
+	Sääsel AS Sää,
+	Lämpötila
+FROM
+    Onnettomuudet o
+INNER JOIN Tieomin t
+    ON o.tieomin_id = t.tieomin_id AND
+	o.Vuosi = t.Vuosi
+WHERE
+    Vakavuusko > 0
+GROUP BY 
+    o.Vuosi,
+	Kk,
+	Vkpv,
+	Tunti,
+	Vakavuus,
+	Loukkaant,
+	Kuolleet,
+	Onlksel,
+	Ontyypsel,
+	Onnpaiksel,
+	Nopraj,
+	t.Noplajisel,
+	t.Risteys,
+	t.Risteyssel,
+	Pintasel,
+	Valsel,
+	Sääsel,
+	Lämpötila;
+*/
+
+-- CHECK VIEW QUERY
+/*
+SELECT *
+FROM
+   vuos_vak_onnett_olos;
+*/
+
+----------------------------------------------------------------------
+-- QUERY INCIDENT SERIOUS WITH INCIDENT TYPES, CONDITIONS AND LOCATION
+-- WITHOUT MILD INCIDENT
+
+/*
+CREATE OR ALTER VIEW vuos_vak_onnett_paikka
+AS
+SELECT
+    o.Vuosi,
+	Kk AS Kuukausi,
+	Vkpv AS Viikonpäivä,
+	Tunti,
+	Vakavuus,
+	Loukkaant AS Loukkaantuneet,
+	Kuolleet,
+	Onlksel AS Onnett_Luokka,
+	Ontyypsel AS Onnett_Tyyppi,
+	Onnpaiksel AS Onnett_Paikka,
+	Nopraj AS Nopeusrajoitus,
+	Pintasel AS Tienpinta,
+	Valsel AS Valoisuus,
+	Sääsel AS Sää,
+	Lämpötila,
+	a.Maakuntsel AS Maakunta,
+	Väestö,
+	a.Kuntasel AS Kunta,
+	Katuosoite,
+	[position.lat],
+	[position.lon]
+FROM
+    Onnettomuudet o
+INNER JOIN Alueet a
+    ON o.alue_id = a.alue_id
+INNER JOIN Väestö v
+    ON o.Vuosi = v.Vuosi
+WHERE
+	a.Maakuntsel = v.Maakuntsel AND
+    Vakavuusko > 0
+GROUP BY 
+    o.Vuosi,
+	Kk,
+	Vkpv,
+	Tunti,
+	Vakavuus,
+	Loukkaant,
+	Kuolleet,
+	Onlksel,
+	Ontyypsel,
+	Onnpaiksel,
+	Nopraj,
+	Pintasel,
+	Valsel,
+	Sääsel,
+	Lämpötila,
+	a.Maakuntsel,
+	Väestö,
+	a.Kuntasel,
+	Katuosoite,
+	[position.lat],
+	[position.lon];
+*/
+
+-- CHECK VIEW QUERY
+/*
+SELECT *
+FROM
+   vuos_vak_onnett_paikka;
+*/
+
+----------------------------------------------------------------------
+-- QUERY INCIDENT SERIOUS WITH INCIDENT TYPES, CONDITIONS AND LOCATION
+-- WITH ALL INCIDENT
+
+/*
+CREATE OR ALTER VIEW vuos_onnett_paikka
+AS
+SELECT
+    o.Vuosi,
+	Kk AS Kuukausi,
+	Vkpv AS Viikonpäivä,
+	Tunti,
+	Vakavuus,
+	Loukkaant AS Loukkaantuneet,
+	Kuolleet,
+	Onlksel AS Onnett_Luokka,
+	Ontyypsel AS Onnett_Tyyppi,
+	Onnpaiksel AS Onnett_Paikka,
+	Nopraj AS Nopeusrajoitus,
+	Pintasel AS Tienpinta,
+	Valsel AS Valoisuus,
+	Sääsel AS Sää,
+	Lämpötila,
+	a.Maakuntsel AS Maakunta,
+	Väestö,
+	a.Kuntasel AS Kunta,
+	Katuosoite,
+	[position.lat],
+	[position.lon]
+FROM
+    Onnettomuudet o
+INNER JOIN Alueet a
+    ON o.alue_id = a.alue_id
+INNER JOIN Väestö v
+    ON o.Vuosi = v.Vuosi
+WHERE
+	a.Maakuntsel = v.Maakuntsel
+GROUP BY 
+    o.Vuosi,
+	Kk,
+	Vkpv,
+	Tunti,
+	Vakavuus,
+	Loukkaant,
+	Kuolleet,
+	Onlksel,
+	Ontyypsel,
+	Onnpaiksel,
+	Nopraj,
+	Pintasel,
+	Valsel,
+	Sääsel,
+	Lämpötila,
+	a.Maakuntsel,
+	Väestö,
+	a.Kuntasel,
+	Katuosoite,
+	[position.lat],
+	[position.lon];
+*/
+
+-- CHECK VIEW QUERY
+/*
+SELECT *
+FROM
+   vuos_onnett_paikka;
+*/
