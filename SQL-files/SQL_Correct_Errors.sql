c6b5df0598bd6aa87d4a2204e142c4a46c1f780f
pasihintikka <110663840+pasihintikka@users.noreply.github.com>
2022-08-18 10:37:30 +0300

    Add files via upload

---
+++ SQL-files/SQL_Correct_Errors.sql
@@ -0,0 +1,154 @@
+--USE Onnettomuudet
+
+
+----------------------
+-- CHANGE COLUMN LENGTH
+
+--ALTER TABLE dbo.Onnettomuudet ALTER COLUMN Vkpv VARCHAR(15);
+
+
+------------------------
+-- DROP TABLES IF NEEDED
+
+--DROP TABLE dbo.Onnettomuudet;  
+--GO
+
+--DROP TABLE dbo.Alueet;  
+--GO
+
+--DROP TABLE dbo.Tieomin;  
+--GO
+
+--DROP TABLE dbo.Osalliset;  
+--GO
+
+--DROP TABLE dbo.Väestö;  
+--GO
+
+
+-----------------------------
+-- DELETE ALL DATA FROM TABLE
+
+--DELETE FROM Osalliset;
+
+-- DELETE DATA FROM TABLE BASED ON A CRITERIA
+
+--DELETE FROM Onnettomuudet WHERE  Vuosi < 2009;
+
+
+-- SELECT DATA WITH CRITERIA
+
+--SELECT Onnett_id, Vuosi, [position.lat], [position.lon], Katuosoite FROM Onnettomuudet WHERE [position.lon] > 40 ORDER BY Vuosi;
+
+----------------------------------------------------------
+-- SELECT AND UPDATE NEGATIVE VALUES (NOT ALLOWED COLUMNS)
+
+/*
+SELECT Onnett_id, Lämpötila
+FROM Onnettomuudet
+WHERE Lämpötila IS NULL
+GROUP BY Onnett_id, Lämpötila;
+*/
+
+--UPDATE Onnettomuudet SET Nopraj = NULL WHERE Nopraj <= 0;
+
+--UPDATE Onnettomuudet SET Katuosoite = NULL WHERE Onnett_id = 6684845;
+
+--UPDATE Onnettomuudet SET Pinta = NULL WHERE Pinta <= 0;
+
+--UPDATE Onnettomuudet SET Sää = NULL WHERE Sää <= 0;
+
+--UPDATE Onnettomuudet SET Valoisuus = NULL WHERE Valoisuus <= 0;
+
+--UPDATE Onnettomuudet SET Tunti = NULL WHERE Tunti < 0;
+
+--UPDATE Osalliset SET Peräv = NULL WHERE Peräv < 0;
+
+--UPDATE Osalliset SET Ajolaji = NULL WHERE Ajolaji < 0;
+
+--UPDATE Osalliset SET Kuollut = 0 WHERE Kuollut IS NULL;
+
+--UPDATE Osalliset SET Loukk = 0 WHERE Loukk IS NULL;
+
+--UPDATE Osalliset SET Ajoneuvika = 0 WHERE Ajoneuvika IS NULL;
+
+/*
+SELECT Osall_id, Kuollut, Loukk, Ajoneuvika
+FROM Osalliset
+WHERE Kuollut IS NULL
+GROUP BY Osall_id, Kuollut, Loukk, Ajoneuvika;
+*/
+
+
+------------------------------------------------------------------------
+-- CREATE A TABLE TEMP_ONN_COORD FOR UPDATING SOME CORRECTED COORDINATES
+
+/*
+CREATE TABLE Temp_Onn_Coord  
+   (Onnett_id INT PRIMARY KEY NOT NULL,
+   [position.lat] NUMERIC(18,9),
+   [position.lon] NUMERIC(18,9));
+*/
+
+/*
+INSERT INTO Temp_Onn_Coord(Onnett_id, [position.lat], [position.lon]) 
+VALUES 
+(6431907,65.204661052,25.546662867),
+(6502988,65.054843071,25.48537159),
+(6580257,65.053328842,25.494511352),
+(6652181,65.090591402,25.63593325),
+(6709860,60.776581246,25.460115123),
+(6514438,64.600200858,26.572252499),
+(6537791,64.996936542,25.512934817),
+(6555192,NULL,NULL),
+(6567107,64.677294663,26.512409028),
+(6597827,64.768124384,28.160966245),
+(6630943,65.090109766,25.512735902),
+(6639532,NULL,NULL),
+(6647156,60.685535201,25.348681529),
+(6678938,65.341294459,26.927224521),
+(6704622,64.934685834,28.815400613),
+(6748903,64.726034312,29.682790386),
+(6748951,NULL,NULL),
+(6776215,NULL,NULL),
+(6823553,60.780978096,25.436523769),
+(6823781,64.841502922,25.351356139),
+(6823811,60.582736107,25.205084422),
+(6908229,60.624650687,25.206538919),
+(6434291,65.456443756,29.045915739),
+(6501694,NULL,NULL),
+(6558115,62.161695198,27.262171997),
+(6580732,65.208837353,25.427509309),
+(6580930,63.097131177,21.68349002),
+(6582838,63.992741881,24.839801362),
+(6589380,65.875343341,29.248476769),
+(6626556,NULL,NULL),
+(6627149,65.070463949,25.797367953),
+(6661343,65.146444856,28.929984526),
+(6680574,65.171715959,25.669612314),
+(6684845,65.649778337,25.361707012),
+(6714540,60.735368527,25.421172207),
+(6743878,65.376225273,25.762530384),
+(6773262,65.045248302,26.935084896),
+(6787382,64.157716076,28.044708531),
+(6846592,61.55727915,25.827222594),
+(6888638,65.232309131,25.482837766);
+*/
+
+--SELECT * FROM Temp_Onn_Coord;
+
+/*
+UPDATE Onnettomuudet
+SET 
+Onnettomuudet.[position.lat]=Temp_Onn_Coord.[position.lat], 
+Onnettomuudet.[position.lon]=Temp_Onn_Coord.[position.lon]
+FROM Onnettomuudet
+INNER JOIN Temp_Onn_Coord
+ON Onnettomuudet.Onnett_id = Temp_Onn_Coord.Onnett_id
+*/
+
+--DELETE FROM Temp_Onn_Coord
+
+--DROP TABLE dbo.Temp_Onn_Coord;  
+--GO
+
---
+++ SQL-files/SQL_Test_Queries.sql
@@ -0,0 +1,135 @@
+--USE Onnettomuudet
+
+
+--------------------------------------------------------------------------------
+-- QUERY INCIDENT SERIOUS WITH INCIDENT TYPES, INVOLVED, CONDITIONS AND LOCATION
+-- WITHOUT MILD INCIDENT
+
+/*
+SELECT
+    o.Vuosi,
+	Kk AS Kuukausi,
+	Vkpv AS Viikonpäivä,
+	Tunti,
+	Vakavuus,
+	os.Kuollut AS Kuolleita,
+	os.Loukk AS Loukkaantuneita,
+	os.Oslajisel AS Osallisen_laji,
+	Onlksel AS Onnett_Luokka,
+	Ontyypsel AS Onnett_Tyyppi,
+	Onnpaiksel AS Onnett_Paikka,
+	Nopraj AS Nopeusrajoitus,
+	t.Noplajisel AS NopRaj_Laji,
+	t.Risteys AS Risteävien_lkm,
+	t.Risteyssel AS Risteys_Laji,
+	Pintasel AS Tienpinta,
+	Valsel AS Valoisuus,
+	Sääsel AS Sää,
+	Lämpötila,
+	a.Maakuntsel AS Maakunta,
+	a.Kuntasel AS Kunta,
+	Katuosoite,
+	[position.lat],
+	[position.lon]
+FROM
+    Onnettomuudet o
+INNER JOIN Alueet a 
+    ON o.alue_id = a.alue_id
+INNER JOIN Tieomin t
+    ON o.tieomin_id = t.tieomin_id AND
+	o.Vuosi = t.Vuosi
+INNER JOIN Osalliset os
+    ON o.Onnett_id = os.Onnett_id
+WHERE
+    Vakavuusko > 0
+GROUP BY 
+    o.Vuosi,
+	Kk,
+	Vkpv,
+	Tunti,
+	Vakavuus,
+	os.Kuollut,
+	os.Loukk,
+	os.Oslajisel,
+	Onlksel,
+	Ontyypsel,
+	Onnpaiksel,
+	Nopraj,
+	t.Noplajisel,
+	t.Risteys,
+	t.Risteyssel,
+	Pintasel,
+	Valsel,
+	Sääsel,
+	Lämpötila,
+	a.Maakuntsel,
+	a.Kuntasel,
+	Katuosoite,
+	[position.lat],
+	[position.lon];
+*/
+
+--------------------------------------------
+-- NUMERIC DATA FOR HEATMAP WITHOUT LOCATION
+
+/*
+SELECT
+    o.Vuosi,
+	Kk AS Kuukausi,
+	Vkpv AS Viikonpäivä,
+	Tunti,
+	Vakavuusko AS Vakavuus,
+	os.Kuollut AS Kuolleita,
+	os.Loukk AS Loukkaantuneita,
+	os.Oslaji AS Osallisen_laji,
+	Onluokka AS Onnett_Luokka,
+	Ontyyppi AS Onnett_Tyyppi,
+	Onnpaikka AS Onnett_Paikka,
+	Nopraj AS Nopeusrajoitus,
+	t.Noplaji AS NopRaj_Laji,
+	t.Risteys AS Risteävien_lkm,
+	t.Talvhoitlk AS Talvihoitoluokka,
+	Pinta AS Tienpinta,
+	Valoisuus,
+	Sää,
+	Lämpötila,
+	a.Maakunta,
+	a.Kunta,
+	v.Väestö
+FROM
+    Onnettomuudet o
+INNER JOIN Alueet a 
+    ON o.alue_id = a.alue_id
+INNER JOIN Tieomin t
+    ON o.tieomin_id = t.tieomin_id AND
+	o.Vuosi = t.Vuosi
+INNER JOIN Osalliset os
+    ON o.Onnett_id = os.Onnett_id
+INNER JOIN Väestö v
+    ON o.Vuosi = v.Vuosi
+WHERE
+    a.Maakunta = v.Maakunta
+GROUP BY 
+    o.Vuosi,
+	Kk,
+	Vkpv,
+	Tunti,
+	Vakavuusko,
+	os.Kuollut,
+	os.Loukk,
+	os.Oslaji,
+	Onluokka,
+	Ontyyppi,
+	Onnpaikka,
+	Nopraj,
+	t.Noplaji,
+	t.Risteys,
+	t.Talvhoitlk,
+	Pinta,
+	Valoisuus,
+	Sää,
+	Lämpötila,
+	a.Maakunta,
+	a.Kunta,
+	v.Väestö;
+*/
